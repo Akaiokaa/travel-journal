ade6458e714a9117f834c588156488645075fd40
Akai <161771111+Akaiokaa@users.noreply.github.com>
2026-04-19 00:13:11 -0700

    globe path change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import './Header.css'
 function Header () {
     return (
         <header>
-            <img src="public/globe.png" alt="Picture of a globe" />
+            <img src="globe.png" alt="Picture of a globe" />
             <h1>My travel journal</h1>
         </header>
     ) 
